f40f813bdafac504d3b21267bda9321ff1166d4f
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-10-26 22:41:40 +0000

    50percent complete

--- app.rb
+++ app.rb
@@ -7,3 +7,44 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/hi") do
+  "<h1>Hi Friend!</h1>"
+  
+end
+
+#Square Code --------------------------------------------------------
+
+get("/square/new") do
+  erb(:square)
+  end
+
+get("/square/results") do
+  @the_num = params.fetch("number").to_f
+  @theresult = (@the_num ** 2.0)
+  erb(:square_results)
+end
+
+#Square Root Code ---------------------------------------------------
+
+get("/square_root/new") do
+  erb(:squareroot)
+  end
+
+get("/square_root/results") do
+  @the_num = params.fetch("number").to_f
+  @theresult = Math.sqrt(@the_num)
+  erb(:square_root_results)
+end
+
+#Payment Code ---------------------------------------------------
+
+get("/payment/new") do
+  erb(:payment)
+  end
+
+get("/payment/results") do
+  #@the_num = params.fetch("number").to_f
+  #@theresult = Math.sqrt(@the_num)
+  erb(:payment_results)
+end
